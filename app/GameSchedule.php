8ba363c4c7e22c8bbd8153ba3eea46a29683f9fa
nirakka <ikoa.syo@gmail.com>
2018-05-26 18:59:25 +0900

    add relation

--- app/GameSchedule.php
+++ app/GameSchedule.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class GameSchedule extends Model
 {
     //
+    protected $fillable = ['start_time','place','team_a_id', 'team_b_id' ];
+
+    public function teamA(){
+        return $this->belongsTo('App\Team', 'team_a_id','id');
+    }
+
+    public function teamB(){
+        return $this->belongsTo('App\Team','team_b_id', 'id');
+    }
 }
