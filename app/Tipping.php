1534c925f325941ae787ed78c94540e02387093f
nirakka <ikoa.syo@gmail.com>
2018-05-26 18:47:31 +0900

    add relation

--- app/Tipping.php
+++ app/Tipping.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Tipping extends Model
 {
     //
+    protected $fillable = ['game_id','user_id', 'team_id', 'money'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function team(){
+        return $this->belongsTo('App\Team');
+    }
+
+    public function gam(){
+        return $this->belongsTo('App\GameSchedule','game_id', 'id');
+    }
 }
